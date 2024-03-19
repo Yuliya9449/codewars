150d998c38df5cda1e1eb25cd02d7cff68b1ecb1
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-20 01:34:25 +0300

    feat: add solution Sum Arrays task

--- ten1/script.js
+++ ten1/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // ! 1
 // Days in the year
 // https://www.codewars.com/kata/56d6c333c9ae3fc32800070f
@@ -6,4 +7,12 @@ function yearDays(year) {
   const isLeapYear = (!(year % 4) && !!(year % 100)) || !(year % 400);
 
   return `${year} has ${isLeapYear ? 366 : 365} days`;
+}
+
+// ! 2
+// Sum Arrays
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+
+function sum(numbers) {
+  return numbers.reduce((acc, elem) => acc + elem, 0);
 }
